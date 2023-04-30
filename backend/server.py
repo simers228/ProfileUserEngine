cba16806bef8900c27506832c41cf851b9d144fc
CalvinismConvert <calvinsopocy@gmail.com>
2023-04-30 17:18:38 -0600

    Some more slight changes and fixes
    fixed some things all visual and added elements.

--- backend/server.py
+++ backend/server.py
@@ -1,18 +1,16 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, Response
 import subprocess
 
-app = Flask(__name__, static_folder="../frontend/build/static", template_folder="../frontend/build")
+app = Flask(__name__)
 # DO NOT FORGET TO ADD TEXT FILTERING TO AVOID INJECTION
 @app.route('/', methods=['GET', 'POST'])
 def server():
     if request.method == 'POST':
         URL = request.form['linkedinUrl']
         subprocess.run(['python', './ProfileUserEngine-main/main.py', URL])
-        return render_template("index.html")
+        return Response(status=204)
     else:
         return 'Failure'
-
-
 
 if __name__== "__main__":
     app.run(debug=True)
