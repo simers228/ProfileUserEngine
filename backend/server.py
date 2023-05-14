4b35cea53940e103ca07bb3c8dddb8e54e594c75
CalvinismConvert <calvinsopocy@gmail.com>
2023-05-14 17:09:49 -0600

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, Response, jsonify, session
+from flask import Flask, request, Response, jsonify
 from scripts.linkedInScraper import LinkedInScraper 
 from scripts.filterCandidates import FilterClass
 #from scrapy.crawler import CrawlerProcess
@@ -16,42 +16,32 @@ app.secret_key = 'WebScraping'
 
 @app.route('/userengine', methods=['GET', 'POST'])
 def linkedin():
+    usernames = ['allisonbentley', 'andrea-wilkins-aa717b151', 'ben-hruban-b18b51b6', 'colin-bailey-a618a622', 'corey-buck-leed-ap-27508910a', 'derrick-hensel-0886996a', 'dustin-parish-a0434468', 'j-d-eickbush-53861456', 'jeremy-just-b0003441', 'jessica-florez-909b15a', 'joe-buelt-68923875', 'jon-timperley-454a2754', 'kendal-fuller-4998b797', 'kevin-sladovnik-12a8a72a', 'shane-rothchild-a18610106']
     if request.method == 'POST':
         URL = request.form['linkedinUrl']
         scraper = LinkedInScraper(URL)
         usernames = scraper.process()
-        print(usernames)
-        session['usernames'] = usernames
         return Response(status=204)
     elif request.method == 'GET':
-        usernames = session.get('usernames')
+        print(usernames)
         return jsonify(usernames)
     return Response(status=204)
 @app.route('/recruiter', methods=['GET', 'POST'])
 def recruiter():
+    global job_position, location, job_description, domain
     if request.method == 'POST':
         if request.headers.get('X-Request-ID') == "User-Input":
             job_position = request.json.get('jobPosition')
-            session['job_position'] = job_position
             location = request.json.get('location')
-            session['location'] = location
             job_description = request.json.get('jobDescription')
-            session['job_description'] = job_description
             domain = request.json.get('domain')
-            session['domain'] = domain
             if location == None:
                 location = ""
-            print(f'job_position: {job_position}, location: {location}, job_description: {job_description}, domain: {domain}')
-            usernames = session.get('usernames')
             os.chdir('./RecruiterAI/')
             subprocess.Popen(['scrapy', 'crawl', 'linkedin_people_profile'])
             os.chdir('../')
             return Response(status=204)
         else:
-            job_position = session.get('job_position')
-            location = session.get('location')
-            job_description = session.get('job_description')
-            domain = session.get('domain')
             print(f'job_position: {job_position}, location: {location}, job_description: {job_description}, domain: {domain} + cunt')
             item = request.get_json()
             filterObject = FilterClass(item, job_position, location, job_description, domain)
