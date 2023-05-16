397957ee0551069bf47f6a2a5880633cff3710f7
aidang1003 <60905689+aidang1003@users.noreply.github.com>
2023-05-15 20:32:36 -0600

    Run this

--- backend/app.py
+++ backend/app.py
@@ -41,7 +41,7 @@ def recruiter():
     global job_position, location, job_description, domain
     if request.method == 'POST':
         if request.headers.get('X-Request-ID') == "User-Input":
-            user = 'Calvin2'
+            user = 'Calvin3'
             job_position = request.json.get('jobPosition')
             location = request.json.get('location')
             job_description = request.json.get('jobDescription')
@@ -67,18 +67,14 @@ def recruiter():
             os.chdir('../')
             return Response(status=204)
         else:
-            # user = user.query.order_by(User.id.desc()).first()
-            # job_position = user.job_position
-            # location = user.location
-            # job_description = user.job_description
-            # domain = user.domain
-
             myConn = PostgresConnection()
             myConn.connect()
+            # userList = myConn.selectStatement(f"SELECT * FROM tbl_recruiteroptions where charuser = \'{user}\'")
             userList = myConn.selectStatement(
-                f"SELECT * FROM tbl_recruiteroptions where charuser = \'{user}\'")
-            myConn.disconnect()
+                f"SELECT * FROM tbl_recruiteroptions where charuser = \'Calvin3\'")
 
+            myConn.disconnect()
+            print("select list return > \n")
             print(userList)
 
             print(
